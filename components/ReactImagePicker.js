d31c7a769c8fc0f8a16e9d07f16764943c82dda4
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2024-08-26 21:35:42 +0530

    create login page

--- components/ReactImagePicker.js
+++ components/ReactImagePicker.js
@@ -82,25 +82,29 @@
 
 import React from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 export default function LoginScreen() {
   return (
     <View style={styles.container}>
-      <Image source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}} style={styles.logo} />
+      <Image source={require('../assets/lens-icon.png')} resizeMode="contain" style={styles.logo3} />
+      <Image source={{ uri: 'https://www.gameartguppy.com/wp-content/uploads/2019/04/mascot_firebase-logo.png' }} style={styles.logo} />
+      <Image source={require('../assets/react-icon.png')} resizeMode="contain" style={styles.logo2} />
       <Text style={styles.title}>RN Social App</Text>
-      
-      <TextInput 
+
+      <TextInput
         placeholder="Email"
         style={styles.input}
         keyboardType="email-address"
         autoCapitalize="none"
       />
-      <TextInput 
+      <TextInput
         placeholder="Password"
         style={styles.input}
         secureTextEntry
       />
-      
+
       <TouchableOpacity style={styles.signInButton}>
         <Text style={styles.signInText}>Sign In</Text>
       </TouchableOpacity>
@@ -108,15 +112,17 @@ export default function LoginScreen() {
       <TouchableOpacity>
         <Text style={styles.forgotPassword}>Forgot Password?</Text>
       </TouchableOpacity>
-      
+
       <TouchableOpacity style={styles.facebookButton}>
+        <FontAwesome style={{ position: 'relative', right: 70 }} name="facebook-f" size={20} color="#3b5998" />
         <Text style={styles.facebookText}>Sign In with Facebook</Text>
       </TouchableOpacity>
-      
+
       <TouchableOpacity style={styles.googleButton}>
+        <AntDesign style={{ position: 'relative', right: 80 }} name="google" size={20} color="#db4a39" />
         <Text style={styles.googleText}>Sign In with Google</Text>
       </TouchableOpacity>
-      
+
       <TouchableOpacity>
         <Text style={styles.createAccount}>Don't have an account? Create here</Text>
       </TouchableOpacity>
@@ -127,20 +133,35 @@ export default function LoginScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     paddingHorizontal: 20,
     backgroundColor: '#fff',
   },
   logo: {
+    width: 200,
+    height: 200,
+  },
+  logo2: {
     width: 80,
     height: 80,
-    marginBottom: 20,
+    position: 'relative',
+    bottom: 90,
+    right: 60
+  },
+  logo3: {
+    width: 40,
+    zIndex:999,
+    height: 40,
+    position: 'relative',
+    top:65,
+    left:45
   },
   title: {
-    fontSize: 24,
+    fontSize: 40,
     fontWeight: 'bold',
     marginBottom: 30,
+    color: 'red',
+    marginTop:-80
   },
   input: {
     width: '100%',
@@ -171,28 +192,34 @@ const styles = StyleSheet.create({
   facebookButton: {
     width: '100%',
     height: 50,
-    backgroundColor: '#3b5998',
+    backgroundColor: '#d0efff',
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 8,
     marginBottom: 10,
+    display: 'flex',
+    flexDirection: 'row',
   },
   facebookText: {
-    color: '#fff',
+    color: '#3b5998',
     fontSize: 18,
+    fontWeight: '700'
   },
   googleButton: {
     width: '100%',
     height: 50,
-    backgroundColor: '#db4a39',
+    backgroundColor: '#ffcbd1',
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 8,
     marginBottom: 30,
+    display: 'flex',
+    flexDirection: 'row',
   },
   googleText: {
-    color: '#fff',
+    color: '#db4a39',
     fontSize: 18,
+    fontWeight: '700'
   },
   createAccount: {
     color: '#007bff',
